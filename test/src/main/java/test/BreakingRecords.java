4057f6952846ae0f4d287feaaa26965bd768366b
hevani <hayagreev.evani@dlhcorp.com>
2025-12-13 21:40:53 -0500

    added breakingRecords logic

---
+++ test/src/main/java/test/BreakingRecords.java
@@ -0,0 +1,77 @@
+package test;
+
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Result4 {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+		
+    // Write your code here
+		List<Integer> result = new ArrayList<>();
+		int maxCount = 0;
+		int minCount =0;
+		
+		int max = scores.get(0);
+		int min = scores.get(0);
+		
+		for(int score: scores) {
+			if(score > max) {
+				max = score;
+				maxCount++;
+			}
+			else if(score < min) {
+				min = score;
+				minCount++;
+			}
+			
+		}
+		
+		result.add(maxCount);
+		result.add(minCount);
+		return result;
+    }
+
+}
+
+public class BreakingRecords {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result4.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
