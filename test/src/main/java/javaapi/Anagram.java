5b942865b94771db37a47629d4cdba5dbf74a0b8
hevani <hayagreev.evani@dlhcorp.com>
2026-01-02 18:35:50 -0500

    Modified Anagram method to return string as per the HackerRank and used Scanner to read input lines

--- test/src/main/java/javaapi/Anagram.java
+++ test/src/main/java/javaapi/Anagram.java
@@ -1,16 +1,18 @@
 package javaapi;
 
+import java.util.Scanner;
+
 public class Anagram {
 
-    private static boolean isAnagram(String a, String b) {
+	private static String isAnagram(String a, String b) {
+        String isAnagram = "Not Anagrams";
         a = a.toLowerCase();
         b = b.toLowerCase();
 
         if (a.length() != b.length()) {
-            return false;
+            return isAnagram;
         }
-
-        // Frequency array
+        
         int[] freq = new int[26];
 
         // Count characters in a
@@ -22,21 +24,21 @@ public class Anagram {
         for (char c : b.toCharArray()) {
             freq[c - 'a']--;
         }
-
-        // All counts must be zero
+        
         for (int count : freq) {
             if (count != 0) {
-                return false;
+                return isAnagram;
             }
         }
 
-        return true;
+        return "Anagrams";
     }
 
     public static void main(String[] args) {
-        System.out.println(isAnagram("anagram", "margana"));   // true
-        System.out.println(isAnagram("CAT", "cat"));           // true
-        System.out.println(isAnagram("CAT", "taC"));           // true
-        System.out.println(isAnagram("anagramm", "marganaa")); // false
+    	 Scanner sc = new Scanner(System.in);
+         
+         String a = sc.nextLine();
+         String b = sc.nextLine();
+         System.out.println(isAnagram(a,b));
     }
 }
